de461374c89f16e21beebb940e7f723290115e03
Incomplite <blocktapok@gmail.com>
2025-05-17 19:46:40 +0300

    Update env.zsh

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,4 +1,8 @@
 # fzf
-export FZF_DEFAULT_OPTS="--walker=file,dir,hidden --walker-skip=.git --preview 'bat --style=numbers --color=always --line-range :500 {}'"
+export FZF_DEFAULT_OPTS="--walker=file,dir,hidden \
+--walker-skip=.git \
+--preview 'bat --style=numbers --color=always {}' \
+--preview-window=right:60% \
+--bind 'ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down,ctrl-b:preview-page-up,ctrl-f:preview-page-down'"
 
 [ -f "$HOME/.local/bin/env" ] && . "$HOME/.local/bin/env"
